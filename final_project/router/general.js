2287a74594ca4e77ebfbe6618c702a7f06471ab8
mic-tar <162429370+mic-tar@users.noreply.github.com>
2024-03-11 17:08:31 +0100

    refactor: use async book requests

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const axios = require("axios").default;
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
@@ -19,39 +20,94 @@ public_users.post("/register", (req, res) => {
 });
 
 // Get the book list available in the shop
-public_users.get("/", function (req, res) {
+let url = "some remote url";
+public_users.get("/", async function (_req, res) {
+  const result = await axios.get(url).catch(function (error) {
+    if (error.response) {
+      // The request was made and the server responded with a status code that falls out of the range of 2xx
+      return res.status(error.response.status).json(error.response.data);
+    } else if (error.request) {
+      // The request was made but no response was received `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+      // http.ClientRequest in node.js
+      return res.status(500).json({ message: "The request was made but no response was received" });
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      return res.status(500).json({ message: error.message });
+    }
+  });
+
+  const books = result.data;
   return res.status(200).json(books);
 });
 
 // Get book details based on ISBN
-public_users.get("/isbn/:isbn", function (req, res) {
-  const isbn = req.params.isbn;
-  if (!isbn in books) {
-    return res.status(404).json({ message: `Isbn ${isbn} not found` });
+public_users.get("/isbn/:isbn", async function (req, res) {
+  function getBookByIsbn(isbn) {
+    return new Promise(function (resolve, reject) {
+      if (isbn in books) {
+        resolve(books[isbn]);
+      }
+      reject(`Isbn ${isbn} not found`);
+    });
   }
-  return res.status(200).json(books[isbn]);
+
+  getBookByIsbn(req.params.isbn)
+    .then((book) => {
+      return res.status(200).json(book);
+    })
+    .catch((err) => {
+      return res.status(400).json({ message: err });
+    });
 });
 
 // Get book details based on author
 public_users.get("/author/:author", function (req, res) {
-  const booksFilteed = {};
-  Object.entries(books).forEach(([isbn, book]) => {
-    if (book.author === req.params.author) {
-      booksFilteed[isbn] = book;
-    }
+  function getBooksByAutor(autor) {
+    return new Promise(function (resolve, reject) {
+      if (!autor) {
+        reject("No author provided.");
+      }
+      const booksFilteed = {};
+      Object.entries(books).forEach(([isbn, book]) => {
+        if (book.author === req.params.author) {
+          booksFilteed[isbn] = book;
+        }
+        resolve(booksFilteed);
+      });
+    });
+  }
+  getBooksByAutor(req.params.author)
+  .then((booksFilteed) => {
+    return res.status(200).json(booksFilteed);
+  })
+  .catch((err) => {
+    return res.status(400).json({ message: err });
   });
-  return res.status(200).json(booksFilteed);
 });
 
 // Get all books based on title
 public_users.get("/title/:title", function (req, res) {
-  const booksFilteed = {};
-  Object.entries(books).forEach(([isbn, book]) => {
-    if (book.title === req.params.title) {
-      booksFilteed[isbn] = book;
-    }
+  function getBooksByTitle(title) {
+    return new Promise(function (resolve, reject) {
+      if (!title) {
+        reject("No title provided.");
+      }
+      const booksFilteed = {};
+      Object.entries(books).forEach(([isbn, book]) => {
+        if (book.title === req.params.title) {
+          booksFilteed[isbn] = book;
+        }
+        resolve(booksFilteed);
+      });
+    });
+  }
+  getBooksByTitle(req.params.author)
+  .then((booksFilteed) => {
+    return res.status(200).json(booksFilteed);
+  })
+  .catch((err) => {
+    return res.status(400).json({ message: err });
   });
-  return res.status(200).json(booksFilteed);
 });
 
 //  Get book review
