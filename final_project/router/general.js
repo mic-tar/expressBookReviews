bad3930db6254c04151fa6507984e8fb4d750a47
mic-tar <162429370+mic-tar@users.noreply.github.com>
2024-03-11 17:07:25 +0100

    feat: add book API

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -11,33 +11,48 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get("/", function (req, res) {
+  return res.status(200).json(books);
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
-  
+public_users.get("/isbn/:isbn", function (req, res) {
+  const isbn = req.params.isbn;
+  if (!isbn in books) {
+    return res.status(404).json({ message: `Isbn ${isbn} not found` });
+  }
+  return res.status(200).json(books[isbn]);
+});
+
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get("/author/:author", function (req, res) {
+  const booksFilteed = {};
+  Object.entries(books).forEach(([isbn, book]) => {
+    if (book.author === req.params.author) {
+      booksFilteed[isbn] = book;
+    }
+  });
+  return res.status(200).json(booksFilteed);
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get("/title/:title", function (req, res) {
+  const booksFilteed = {};
+  Object.entries(books).forEach(([isbn, book]) => {
+    if (book.title === req.params.title) {
+      booksFilteed[isbn] = book;
+    }
+  });
+  return res.status(200).json(booksFilteed);
 });
 
 //  Get book review
-public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get("/review/:isbn", function (req, res) {
+  const isbn = req.params.isbn;
+  if (!isbn in books) {
+    return res.status(404).json({ message: `Isbn ${isbn} not found` });
+  }
+  return res.status(200).json(books[isbn].reviews);
 });
 
 module.exports.general = public_users;
