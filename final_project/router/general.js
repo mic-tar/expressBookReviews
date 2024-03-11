84ff3ff7ce8df6d0fa28aadb4830c872055eb224
mic-tar <162429370+mic-tar@users.noreply.github.com>
2024-03-11 17:07:39 +0100

    feat: add user registration

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,13 +1,21 @@
-const express = require('express');
+const express = require("express");
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+public_users.post("/register", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  if (!username || !password) {
+    return res.status(404).json({ message: "Unable to register user." });
+  }
+  if (isValid(username)) {
+    return res.status(400).json({ message: "User already exists!" });
+  }
 
-public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  users[username] = password;
+  return res.status(200).json({ message: "User successfully registred. Now you can login" });
 });
 
 // Get the book list available in the shop
